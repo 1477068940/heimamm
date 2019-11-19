8fea871d5344d1638293d749179f03cf45c5c27a
红烧狮子头 <you@example.com>
2019-11-19 11:34:39 +0800

    饿了吗整合

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    黑马面面
   </div>
+  <!-- 路由出口 -->
+  <router-view></router-view>
 </template>
 
 <script>
@@ -17,12 +18,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+// 导入路由
+import router from './router/router.js';
+
+// 导入饿了吗UI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use (ElementUI);
+
+// 创建Vue实例并挂载app的div上
 new Vue({
   render: h => h(App),
 }).$mount('#app')
