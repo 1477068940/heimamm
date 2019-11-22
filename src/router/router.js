46938ac4c184a4efa36e985b67606f5022117395
红烧狮子头 <you@example.com>
2019-11-22 11:49:38 +0800

    首页框架布局

--- src/router/router.js
+++ src/router/router.js
@@ -8,11 +8,15 @@ Vue.use(VueRouter);
 
 // 导入 登录页面组件
 import login from '../views/login/login.vue';
+// 导入 首页页面组件
+import index from '../views/index/index.vue'
 
 // 创建路由规则
 const routes = [
     // 登录组件规则
-    {path:'/login',component:login}
+    {path:'/login',component:login},
+    // 首页组件规则
+    {path:'/index',component:index}
 ];
 
 // 创建路由对象
---
+++ src/views/index/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <el-container class="bodyer">
+      <el-header class="Header">Header</el-header>
+      <el-container class="zhuti">
+        <el-aside class="Aside" width="200px">Aside</el-aside>
+        <el-main class="Main">Main</el-main>
+      </el-container>
+    </el-container>
+</template>
+
+<script>
+export default {
+    name:'index',
+
+}
+</script>
+
+<style lang="less">
+    .bodyer{
+        width:100%;
+        height:100%;
+        .Header{
+            background-color: skyblue;
+        }
+        .Aside{
+            background-color: green;
+        }
+        .Main{
+            background-color: red;
+        }
+    }
+</style>
